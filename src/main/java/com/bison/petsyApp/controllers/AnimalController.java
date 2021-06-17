ae767dbc3db660ed8304dc6023cfc8b840c35d8d
Eron63 <kevinmichaelis@hotmail.fr>
2021-06-17 15:31:19 +0200

    Last version (Goodbye Antoine)

--- src/main/java/com/bison/petsyApp/controllers/AnimalController.java
+++ src/main/java/com/bison/petsyApp/controllers/AnimalController.java
@@ -1,5 +1,56 @@
 package com.bison.petsyApp.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bison.petsyApp.dtos.animal.DeleteAnimalDTO;
+import com.bison.petsyApp.dtos.animal.FindAnimalDTO;
+import com.bison.petsyApp.dtos.animal.PostAnimalDTO;
+import com.bison.petsyApp.dtos.animal.PutAnimalDTO;
+import com.bison.petsyApp.services.AnimalService;
+
+
+@RestController
+@RequestMapping("animaux")
+@CrossOrigin
 public class AnimalController {
+	
+	    @Autowired
+	    AnimalService service;
+
+	    @GetMapping
+	    public List<FindAnimalDTO> findAll() {
+	        return this.service.findAll();
+	    }
+	    
+	    @GetMapping("{id}")
+	    public FindAnimalDTO findById(@PathVariable Long id) {
+	        return this.service.findById(id);
+	    }
+
+	    @PostMapping
+	    public FindAnimalDTO save(@RequestBody PostAnimalDTO postAnimalDTO) {
+	        return this.service.save(postAnimalDTO);
+	    }
+
+	    @PutMapping
+	    public FindAnimalDTO update(@RequestBody PutAnimalDTO putAnimalDTO) {
+	        return this.service.update(putAnimalDTO);
+	    }
+
+	    @DeleteMapping
+	    public void delete(@RequestBody DeleteAnimalDTO deleteAnimalDTO) {
+	        this.service.delete(deleteAnimalDTO);
+	    }
 
 }
--- src/main/java/com/bison/petsyApp/services/AnimalService.java
+++ src/main/java/com/bison/petsyApp/services/AnimalService.java
@@ -1,5 +1,50 @@
 package com.bison.petsyApp.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+
+import com.bison.petsyApp.dtos.animal.DeleteAnimalDTO;
+import com.bison.petsyApp.dtos.animal.FindAnimalDTO;
+import com.bison.petsyApp.dtos.animal.PostAnimalDTO;
+import com.bison.petsyApp.dtos.animal.PutAnimalDTO;
+import com.bison.petsyApp.models.Animal;
+import com.bison.petsyApp.repositories.AnimalRepository;
+
 public class AnimalService {
+	
+	 AnimalRepository repository;
+	 ModelMapper mapper;
+	 
+	 public AnimalService(AnimalRepository repository, ModelMapper mapper) {
+		 this.repository = repository;
+		 this.mapper = mapper;
+	 }
+	 
+	 public List<FindAnimalDTO> findAll() {
+	        List<Animal> Animal = this.repository.findAll();
+	        List<FindAnimalDTO> AnimalDTOS = new ArrayList<>();
+	        Animal.forEach(animal -> AnimalDTOS.add(mapper.map(animal, FindAnimalDTO.class)));
+	        return AnimalDTOS;
+	    }
+
+	    public FindAnimalDTO save(PostAnimalDTO postAnimalDTO) {
+	        Animal Animal = mapper.map(postAnimalDTO, Animal.class);
+	        return mapper.map(this.repository.save(Animal), FindAnimalDTO.class);
+	    }
+
+	    public FindAnimalDTO update(PutAnimalDTO putAnimalDTO) {
+	        Animal Animal = mapper.map(putAnimalDTO, Animal.class);
+	        return mapper.map(this.repository.save(Animal), FindAnimalDTO.class);
+	    }
+
+	    public FindAnimalDTO findById(Long id) {
+	        return mapper.map(this.repository.findById(id).get(), FindAnimalDTO.class);
+	    }
+
+	    public void delete(DeleteAnimalDTO deleteAnimalDTO) {
+	        this.repository.delete(mapper.map(deleteAnimalDTO, Animal.class));
+	    }
 
 }
