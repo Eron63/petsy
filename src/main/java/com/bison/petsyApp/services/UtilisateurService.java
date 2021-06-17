4acd6b73170537fdf216467bc506deac9d7a3719
Administrateur <nawell.ghezali@hotmail.fr>
2021-06-17 12:56:21 +0200

    services

--- src/main/java/com/bison/petsyApp/services/UtilisateurService.java
+++ src/main/java/com/bison/petsyApp/services/UtilisateurService.java
@@ -1,5 +1,48 @@
 package com.bison.petsyApp.services;
 
+import com.bison.petsyApp.dtos.utilisateur.DeleteUtilisateurDTO;
+import com.bison.petsyApp.dtos.utilisateur.FindUtilisateurDTO;
+import com.bison.petsyApp.dtos.utilisateur.PostUtilisateurDTO;
+import com.bison.petsyApp.models.Utilisateur;
+import com.bison.petsyApp.repositories.UtilisateurRepository;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UtilisateurService {
+
+    UtilisateurRepository repository;
+    ModelMapper mapper;
+
+    public UtilisateurService(UtilisateurRepository repository, ModelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public List<FindUtilisateurDTO> findAll() {
+        List<Utilisateur> utilisateurs = this.repository.findAll();
+        List<FindUtilisateurDTO> utilisateursDTOS = new ArrayList<>();
+        utilisateurs.forEach(utilisateur -> utilisateursDTOS.add(mapper.map(utilisateur, FindUtilisateurDTO.class)));
+        return utilisateursDTOS;
+    }
+
+    public FindUtilisateurDTO save(PostUtilisateurDTO postUtilisateurDTO) {
+        Utilisateur utilisateur = mapper.map(postUtilisateurDTO, Utilisateur.class);
+        return mapper.map(this.repository.save(utilisateur), FindUtilisateurDTO.class);
+    }
+
+    public FindUtilisateurDTO update(PostUtilisateurDTO postUtilisateurDTO) {
+        Utilisateur utilisateur = mapper.map(postUtilisateurDTO, Utilisateur.class);
+        return mapper.map(this.repository.save(utilisateur), FindUtilisateurDTO.class);
+    }
+
+    public FindUtilisateurDTO findById(int id) {
+        return mapper.map(this.repository.findById(id).get(), FindUtilisateurDTO.class);
+    }
+
+    public void delete(DeleteUtilisateurDTO deleteClientDTO) {
+        this.repository.delete(mapper.map(deleteClientDTO, Utilisateur.class));
+    }
 
 }
