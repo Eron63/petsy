3f44bf929a6d970b91ea1f46e3462f7f3e5a77e8
Nicolas Mansoure <sami.mansoure@gmail.com>
2021-06-17 14:19:47 +0200

    model type v1

--- src/main/java/com/bison/petsyApp/models/Type.java
+++ src/main/java/com/bison/petsyApp/models/Type.java
@@ -1,5 +1,18 @@
 package com.bison.petsyApp.models;
 
+import java.util.Collection;
+
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 public class Type {
+
+	private int id_type;
+	private String nom_type;
+
+	@OneToMany(mappedBy = "animal")
+	@JsonIgnoreProperties("animal")
+	private Collection<Animal> animaux;
 
 }
