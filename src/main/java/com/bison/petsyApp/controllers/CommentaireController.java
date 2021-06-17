8d53afb896ec3d3c66b01c5aabf495f8582b1963
ValMasse <valmasse@gmx.fr>
2021-06-17 16:27:35 +0200

    controller Commentaire OK

--- src/main/java/com/bison/petsyApp/controllers/CommentaireController.java
+++ src/main/java/com/bison/petsyApp/controllers/CommentaireController.java
@@ -1,10 +1,14 @@
 package com.bison.petsyApp.controllers;
 
+import com.bison.petsyApp.dtos.commentaire.DeleteCommentaireDTO;
+import com.bison.petsyApp.dtos.commentaire.FindCommentaireDTO;
+import com.bison.petsyApp.dtos.commentaire.PostCommentaireDTO;
+import com.bison.petsyApp.dtos.commentaire.PutCommentaireDTO;
 import com.bison.petsyApp.services.CommentaireService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -15,6 +19,24 @@ public class CommentaireController {
     @Autowired
     private CommentaireService service;
 
+    @GetMapping
+    public List<FindCommentaireDTO> findAll() {
+        return this.service.findAll();
+    }
 
+    @PostMapping
+    public FindCommentaireDTO save(@RequestBody PostCommentaireDTO postCommentaireDTO) {
+        return this.service.save(postCommentaireDTO);
+    }
+
+    @PutMapping
+    public FindCommentaireDTO update(@RequestBody PutCommentaireDTO putCommentaireDTO) {
+        return this.service.update(putCommentaireDTO);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody DeleteCommentaireDTO deleteCommentaireDTO) {
+        this.service.delete(deleteCommentaireDTO);
+    }
 
 }
