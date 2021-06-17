c7393bdda81b96ea4a09809aab2374b739107156
ValMasse <valmasse@gmx.fr>
2021-06-17 14:10:15 +0200

    model commentaire OK

--- src/main/java/com/bison/petsyApp/models/Commentaire.java
+++ src/main/java/com/bison/petsyApp/models/Commentaire.java
@@ -1,5 +1,34 @@
 package com.bison.petsyApp.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Commentaire {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_commentaire;
+
+    private String texte_commentaire;
+
+    private Date date_commentaire;
+
+    @ManyToOne
+    @JoinColumn(name = "id_utilisateur")
+    @JsonIgnoreProperties("commentaires")
+    private Utilisateur utilisateur;
+
+    @ManyToOne
+    @JoinColumn(name = "id_petsy")
+    @JsonIgnoreProperties("commentaires")
+    private Petsy petsy;
 }
